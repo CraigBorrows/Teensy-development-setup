dd1060a174e903f350c3f4e7cb3622c0ac604147
craig <Craigborrows@gmail.com>
2025-08-15 22:29:33 +1000

    recursive add libraries

--- utils/TeensyUtils.cmake
+++ utils/TeensyUtils.cmake
@@ -65,9 +65,9 @@ function(add_teensy_cores TARGET TEENSY_BOARD)
     message(STATUS "Added Teensy ${TEENSY_BOARD} cores: ${CORES_DIR}")
 endfunction()
 
-# Function to easily add Teensy libraries
+# Function to easily add Teensy libraries (checks project libs first, then teensy libs)
 function(add_teensy_library TARGET LIBRARY_NAME)
-    set(PROJECT_LIB_DIR ${CMAKE_CURRENT_SOURCE_DIR}/lib/${LIBRARY_NAME})
+    set(PROJECT_LIB_DIR ${CMAKE_CURRENT_SOURCE_DIR}/libs/${LIBRARY_NAME})
     set(TEENSY_LIB_DIR ${TEENSY_ROOT}/libraries/${LIBRARY_NAME})
 
     # Check project libs directory first
@@ -81,27 +81,62 @@ function(add_teensy_library TARGET LIBRARY_NAME)
         message(FATAL_ERROR "Library '${LIBRARY_NAME}' not found in:\n  - ${PROJECT_LIB_DIR}\n  - ${TEENSY_LIB_DIR}")
     endif()
 
-    # Add main library include directories
-    target_include_directories(${TARGET} PRIVATE
-            ${LIB_DIR}
-            ${LIB_DIR}/src
-    )
+    # Always add the main library directory
+    target_include_directories(${TARGET} PRIVATE ${LIB_DIR})
 
-    # Find and add ALL subdirectories as include paths (for complex libraries like SdFat)
-    file(GLOB_RECURSE SUBDIRS LIST_DIRECTORIES true ${LIB_DIR}/*)
-    foreach(SUBDIR ${SUBDIRS})
-        if(IS_DIRECTORY ${SUBDIR})
-            target_include_directories(${TARGET} PRIVATE ${SUBDIR})
-        endif()
-    endforeach()
+    # Check if there's a src folder - if so, only search in there recursively
+    if(EXISTS ${LIB_DIR}/src)
+        message(STATUS "Found src/ folder, adding recursively from src/")
+        target_include_directories(${TARGET} PRIVATE ${LIB_DIR}/src)
 
-    # Find and add library source files
-    file(GLOB_RECURSE LIB_SOURCES
-            ${LIB_DIR}/*.c
-            ${LIB_DIR}/*.cpp
-    )
+        # Add all subdirectories under src/ but exclude unwanted folders
+        file(GLOB_RECURSE SRC_SUBDIRS LIST_DIRECTORIES true ${LIB_DIR}/src/*)
+        foreach(SUBDIR ${SRC_SUBDIRS})
+            if(IS_DIRECTORY ${SUBDIR})
+                # Get the folder name to check if we should exclude it
+                get_filename_component(FOLDER_NAME ${SUBDIR} NAME)
+                string(TOLOWER ${FOLDER_NAME} FOLDER_NAME_LOWER)
 
+                # Skip unwanted directories
+                if(NOT FOLDER_NAME_LOWER MATCHES "^(examples?|tests?|docs?|\.git|\.svn)$")
+                    target_include_directories(${TARGET} PRIVATE ${SUBDIR})
+                endif()
+            endif()
+        endforeach()
+
+        # Find source files in src/
+        file(GLOB_RECURSE LIB_SOURCES
+                ${LIB_DIR}/src/*.c
+                ${LIB_DIR}/src/*.cpp
+        )
+    else()
+        # No src folder, use old behavior but still exclude unwanted folders
+        message(STATUS "No src/ folder found, adding from root")
+
+        # Add subdirectories but exclude unwanted ones
+        file(GLOB SUBDIRS LIST_DIRECTORIES true ${LIB_DIR}/*)
+        foreach(SUBDIR ${SUBDIRS})
+            if(IS_DIRECTORY ${SUBDIR})
+                get_filename_component(FOLDER_NAME ${SUBDIR} NAME)
+                string(TOLOWER ${FOLDER_NAME} FOLDER_NAME_LOWER)
+
+                # Skip unwanted directories
+                if(NOT FOLDER_NAME_LOWER MATCHES "^(examples?|tests?|docs?|\.git|\.svn)$")
+                    target_include_directories(${TARGET} PRIVATE ${SUBDIR})
+                endif()
+            endif()
+        endforeach()
+
+        # Find source files in root
+        file(GLOB_RECURSE LIB_SOURCES
+                ${LIB_DIR}/*.c
+                ${LIB_DIR}/*.cpp
+        )
+    endif()
+
+    # Exclude source files from unwanted directories
     if(LIB_SOURCES)
+        list(FILTER LIB_SOURCES EXCLUDE REGEX ".*(examples?|tests?|docs?)/.*")
         target_sources(${TARGET} PRIVATE ${LIB_SOURCES})
     endif()
 
